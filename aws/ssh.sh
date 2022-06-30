3286d6654e11736ceaa4c04f8fff2d3452db6d67
Bryon Smith <webmastersmith37@gmail.com>
2022-06-30 18:06:19 -0500

    add variables for easy maintenance

--- aws/ssh.sh
+++ aws/ssh.sh
@@ -4,6 +4,9 @@
 # Get ip address from running instance and ssh into it.
 
 # aws amazon assigned ip. ex.. 3.45.8.23
-ipAddress=`aws ec2 describe-instances --instance-ids i-0b2c69b9e2789466e | grep -i publicipaddress | tr -d 'PublicIpAddress: ",'`
+instanceId="i-0b2c69b9e2789466e"
+key="~/.ssh/Bryon_E2_Free_Tier_AWS_Linux.pem"
 
-ssh -i "~/.ssh/Bryon_E2_Free_Tier_AWS_Linux.pem" "ec2-user@$ipAddress"
+ipAddress=`aws ec2 describe-instances --instance-ids $instanceId | grep -i publicipaddress | tr -d 'PublicIpAddress: ",'`
+
+ssh -i "$key" "ec2-user@$ipAddress"
