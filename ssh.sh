ad12f6c04d7837d818b74bdf386e01000a210a6d
Bryon Smith <webmastersmith37@gmail.com>
2022-06-30 14:57:12 -0500

    add notes to ssh script

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,9 @@
 
-# publicdnsname
+#!/usr/bin/bash
+
+# Get ip address from running instance and ssh into it.
+
+# aws amazon assigned ip. ex.. 3.45.8.23
 ipAddress=`aws ec2 describe-instances --instance-ids i-0b2c69b9e2789466e | grep -i publicipaddress | tr -d 'PublicIpAddress: ",'`
 
 ssh -i "~/.ssh/Bryon_E2_Free_Tier_AWS_Linux.pem" "ec2-user@$ipAddress"
